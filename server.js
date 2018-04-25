2b84d6f290d0803ceeac37455591e55ec4a590b7
William <wtpwinberg@gmail.com>
2018-04-25 12:32:17 -0700

    Init

---
+++ server.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const server = express();
+const port = 5550;
+
+mongoose.connect(server('http://localhost:5550/server')
+);
